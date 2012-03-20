a618625a754c88a1587a50e2e039a0646f2aa061
Rob Anderson <rob.anderson@paymentcardsolutions.co.uk>
2012-03-20 10:20:13 +0000

    Minor update to time formatting to somply with the web services we use

--- lib/savon/core_ext/time.rb
+++ lib/savon/core_ext/time.rb
@@ -4,7 +4,7 @@ module Savon
 
       # Returns an xs:dateTime formatted String.
       def xs_datetime
-        strftime "%Y-%m-%dT%H:%M:%S%Z"
+        strftime "%Y-%m-%dT%H:%M:%SZ"
       end
 
     end
